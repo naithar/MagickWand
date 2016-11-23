8370c850db6060026d127c4dc2688151067cdbed
Sergey Minakov <s.minakov@be-interactive.ru>
2016-11-23 15:33:54 +0300

    coverage update

--- common/coverage.sh
+++ common/coverage.sh
@@ -1,6 +1,6 @@
 if [[ "$(uname)" = "Darwin" ]]; then
     echo ">> Starting coverage generation with OSX build"
     xcodebuild -scheme MagickWand -project MagickWand.xcodeproj  -sdk macosx -configuration Release -enableCodeCoverage YES test build test
-    bash <(curl -s https://codecov.io/bash)
+    bash <(curl -s https://codecov.io/bash) -t 9f79d408-2281-4004-b741-c3c66fe97c06
     echo ">> Finished coverage generation"
 fi
