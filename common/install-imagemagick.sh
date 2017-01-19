874e2ea501c9eb1f60e1e633287ef27a5af03e42
Sergey Minakov <devias.naith@gmail.com>
2017-01-19 17:49:28 +0400

    [#9] Fix Travis Build (#10)
    * changed imagemagick download link #9
    
    * Update install-imagemagick.sh

--- common/install-imagemagick.sh
+++ common/install-imagemagick.sh
@@ -1,7 +1,7 @@
 echo ">> Installing ZLIB"
 
 curl -OL https://www.imagemagick.org/download/delegates/zlib-1.2.8.tar.gz
-tar -xzf zlib-1.2.8.tar.gz
+tar xf zlib-1.2.8.tar.gz
 cd zlib-1.2.8
 ./configure --prefix=/usr/local
 make
@@ -9,11 +9,11 @@ sudo make install
 cd ..
 rm -rf zlib-1.2.8*
 
-echo ">> Installing ImageMagick 6.9.6-5"
+echo ">> Installing ImageMagick 6.9.6-8"
 
-curl -OL https://www.imagemagick.org/download/ImageMagick-6.9.6-5.tar.gz
-tar -xzf ImageMagick-6.9.6-5.tar.gz
-cd ImageMagick-6.9.6-5
+curl -OL https://www.imagemagick.org/download/releases/ImageMagick-6.9.6-8.tar.xz
+tar xf ImageMagick-6.9.6-8.tar.xz
+cd ImageMagick-6.9.6-8
 ./configure --prefix=/usr/local --disable-static --with-modules --without-perl --without-magick-plus-plus --with-quantum-depth=8 --disable-openmp --with-gs-font-dir=/usr/local/share/ghostscript/fonts
 make
 sudo make install
