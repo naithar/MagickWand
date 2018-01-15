f565c706476938e9e06496d2ac6676681223c076
Sergey Minakov <naithar@icloud.com>
2018-01-15 23:28:09 +0300

    use sudo

--- common/install-imagemagick.sh
+++ common/install-imagemagick.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ "$(uname -s)" == "Linux" ]]; then 
-      apt-get update && \
+      sudo apt-get update && \
       apt-get install -y build-essential \
             curl \
             cmake \
@@ -17,7 +17,7 @@ curl -O -L https://downloads.sourceforge.net/lcms/lcms2-2.9.tar.gz
 tar xzvf lcms2-2.9.tar.gz
 cd lcms2-2.9
 ./configure --prefix=/usr/local --disable-static && make
-make install
+sudo make install
 cd ./.. && rm -rf lcms2-2.9*
 
 echo "::::::::::::::::: libexif"
@@ -27,7 +27,7 @@ cd libexif-0.6.21
 ./configure --prefix=/usr/local \
             --with-doc-dir=/usr/local/share/doc/libexif-0.6.21 \
             --disable-static && make
-make install
+sudo make install
 cd ./.. && rm -rf libexif-0.6.21*
 
 echo ":::::::::::::::: NASM"
@@ -35,7 +35,7 @@ curl -O -L  http://www.nasm.us/pub/nasm/releasebuilds/2.13.02/nasm-2.13.02.tar.g
 tar xvfz nasm-2.13.02.tar.gz
 cd nasm-2.13.02
 ./configure --prefix=/usr/local && make
-make install
+sudo make install
 cd ./.. && rm -rf nasm-2.13.02*
 
 echo ":::::::::::::::: JPEG"
@@ -47,7 +47,7 @@ cd libjpeg-turbo-1.5.3
             --with-jpeg8            \
             --disable-static        \
             --docdir=/usr/local/share/doc/libjpeg-turbo-1.5.3 && make
-make install
+sudo make install
 cd ./.. && rm -rf libjpeg-turbo-1.5.3*
 
 echo ":::::::::::::::: JASPER"
@@ -62,7 +62,7 @@ cmake -DCMAKE_INSTALL_PREFIX=/usr/local    \
       -DJAS_ENABLE_DOC=NO            \
       -DCMAKE_INSTALL_DOCDIR=/usr/local/share/doc/jasper-2.0.14 \
       ..  && make
-make install
+sudo make install
 cd ./../.. && rm -rf jasper-2.0.14*
 
 echo ":::::::::::: LibPNG"
@@ -70,7 +70,7 @@ curl -O -L  https://downloads.sourceforge.net/libpng/libpng-1.6.34.tar.gz
 tar xvfz libpng-1.6.34.tar.gz
 cd libpng-1.6.34
 ./configure --prefix=/usr/local --disable-static && make
-make install
+sudo make install
 cd ./.. && rm -rf libpng-1.6.34*
 
 echo ":::::::::::: libraw"
@@ -83,7 +83,7 @@ cd LibRaw-0.18.6
             --enable-lcms    \
             --disable-static \
             --docdir=/usr/local/share/doc/libraw-0.18.6 && make
-make install
+sudo make install
 cd ./.. && rm -rf LibRaw-0.18.6*
 
 echo ":::::::::::: LibTIFF"
@@ -91,7 +91,7 @@ curl -O -L  http://download.osgeo.org/libtiff/tiff-4.0.9.tar.gz
 tar xvfz tiff-4.0.9.tar.gz
 cd tiff-4.0.9
 ./configure --prefix=/usr/local && make
-make install
+sudo make install
 cd ./.. && rm -rf tiff-4.0.9*
 
 echo ":::::::::::: giflib"
@@ -99,7 +99,7 @@ curl -O -L https://downloads.sourceforge.net/giflib/giflib-5.1.4.tar.gz
 tar xvfz giflib-5.1.4.tar.gz
 cd giflib-5.1.4
 ./configure --prefix=/usr/local --disable-static && make
-make install
+sudo make install
 cd ./.. && rm -rf giflib-5.1.4*
 
 echo ":::::::::::: libwebp"
@@ -113,7 +113,7 @@ cd libwebp-0.6.1
             --enable-libwebpextras  \
             --enable-swap-16bit-csp \
             --disable-static        && make
-make install
+sudo make install
 cd ./.. && rm -rf libwebp-0.6.1*
 
 echo ":::::::::::: OpenJPEG"
@@ -123,7 +123,7 @@ cd openjpeg-2.3.0
 mkdir -p build && \
 cd       build &&\
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local .. && make
-make install
+sudo make install
 cd ./../.. && rm -rf openjpeg-2.3.0*
 
 echo "::::::::::: FreeType"
@@ -131,7 +131,7 @@ curl -O -L https://downloads.sourceforge.net/freetype/freetype-2.9.tar.gz
 tar zxvf freetype-2.9.tar.gz
 cd freetype-2.9
 ./configure --prefix=/usr/local --disable-static && make
-make install
+sudo make install
 cd ./.. && rm -rf freetype-2.9*
 
 echo "::::::::::::::::: Ghostscript"
@@ -144,8 +144,8 @@ rm -rf freetype lcms2 jpeg libpng zlib
             --enable-dynamic        \
             --with-system-libtiff   && make
 make so
-make install
-make soinstall &&
+sudo make install
+sudo make soinstall &&
 install -v -m644 base/*.h /usr/local/include/ghostscript &&
 ln -v -s ghostscript /usr/local/include/ps
 cd ./.. && rm -rf ghostscript-9.22*
@@ -162,7 +162,7 @@ tar xzvf libwmf-0.2.8.4.tar.gz
 cd libwmf-0.2.8.4
 ./configure --prefix=/usr/local       
 make
-make install
+sudo make install
 cd ./.. && rm -rf libwmf-0.2.8.4*
 
 echo "::::::::::::::::: ImageMagick"
@@ -171,5 +171,5 @@ tar xzvf 6.9.9-33.tar.gz
 cd ImageMagick-6.9.9-33
 ./configure CPPFLAGS='-I/usr/include:/usr/local/include:/usr:/usr/local' LDFLAGS='-L/usr/lib:/usr/local/lib:/usr:/usr/local'   --prefix=/usr/local --disable-static  --with-modules --without-magick-plus-plus --without-perl --without-magick-plus-plus --with-png=yes --with-quantum-depth=16 --with-gs-font-dir=/usr/local/share/ghostscript/fonts --disable-openmp --with-gslib=yes
 make
-make install
+sudo make install
 cd ./.. && rm -rf 6.9.9-33* && rm -rf ImageMagick-*
