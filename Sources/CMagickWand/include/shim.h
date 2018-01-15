a67d71ac0738d8b2d82ea21ca1b2b6682dbd144d
Sergey Minakov <naithar@icloud.com>
2018-01-15 19:50:39 +0300

    possible fix for c lib

---
+++ Sources/CMagickWand/include/shim.h
@@ -0,0 +1,13 @@
+//
+//  shim.h
+//  CMagickWand
+//
+//  Created by Sergey Minakov on 15.01.2018.
+//
+
+#ifndef CMAGICKWAND_SWIFT
+#define CMAGICKWAND_SWIFT
+
+#include <MagickWand.h>
+
+#endif /* shim_h */
